2b98fd0c8bb1eeb808c54f7d9011c9a6da153b29
Ingo Karkat <swdev@ingo-karkat.de>
2023-05-26 12:07:17 +0200

    trace: ENH: Add support for tracing functions
    This is useful to debug some of my wrapper functions (e.g. git, tmux).

---
+++ shell/wrappers.sh
@@ -0,0 +1,7 @@
+#!/bin/sh source-this-script
+
+trace()
+{
+    functionexport "$@" 2>/dev/null # Export any argument with error suppressions; this saves us from duplicating some sort of command-line argument parsing here.
+    command trace "$@"
+}
