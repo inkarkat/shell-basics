fca438bcba419425b7ac644580b4e671770d8a00
Ingo Karkat <swdev@ingo-karkat.de>
2024-09-15 10:46:50 +0200

    ENH: Only offer my own scripts for completion
    I don't usually trace third-party scripts.

--- shell/completions.sh
+++ shell/completions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh source-this-script
 
-completeAsCommand acceptStatus debug debuglog dump-args-exec dump-status fail failThis messageOnStatus negateThis succeedThis teeToStderr trace
+completeAsCommand acceptStatus debug debuglog dump-args-exec dump-status fail failThis messageOnStatus negateThis succeedThis teeToStderr
