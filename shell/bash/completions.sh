3f008e7a25c00e17b0b853d2b66de201470f9907
Ingo Karkat <swdev@ingo-karkat.de>
2024-09-15 11:50:33 +0200

    ENH: Allow enabling tracing for subsidiary (invoked) scripts [, too]

--- shell/bash/completions.sh
+++ shell/bash/completions.sh
@@ -5,12 +5,12 @@ _trace_complete()
     local IFS=$'\n'
     local cur args opts
 
-    opts='-p --paginate --inbox'
+    opts='-f --for -F --only-for -p --paginate --inbox'
     opts="${opts// /$'\n'}"
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
 
-    args="$(mann --commands "$cur")"
+    args="$(MANN_BLACKLIST_FILESPEC=/dev/null mann --commands "$cur")"
 
     readarray -t COMPREPLY < <(compgen -W "${opts}${args:+${opts:+$'\n'}}$args" -- "$cur")
     [ ${#COMPREPLY[@]} -gt 0 ] && readarray -t COMPREPLY < <(printf "%q\n" "${COMPREPLY[@]}")
